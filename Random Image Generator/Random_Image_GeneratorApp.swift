1e12b749151b6dcda00dad39e3316ef1a76c1f2b
Ricky Irfandi <rickyirfandi@gmail.com>
2021-02-27 20:48:21 +0700

    Initial Commit

---
+++ Random Image Generator/Random_Image_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Image_GeneratorApp.swift
+//  Random Image Generator
+//
+//  Created by Irfandi on 27/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Random_Image_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
